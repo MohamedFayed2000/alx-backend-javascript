5d687e4543b45f0a75365d87e5864ef6bbd3cfaa
Mohamed Fayed 2000 <mohamedfayed2000@outlook.com>
2024-04-25 00:17:17 +0000

    promise

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,5 @@
+YourConstructor.prototype.getResponseFromAPI = function() {
+  return new Promise((resolve, reject) => {
+    // Simulating an asynchronous API call with setTimeout
+    setTimeout(() => {}
+}
